15dfc676224ebbf4082742a17e779ba1cec9200f
Fabricio Curvello <fabricio.desenvolvedor@gmail.com>
2024-07-28 12:05:14 -0300

    App completo

--- app/src/main/java/br/com/fabriciocurvello/appremoteprocedurecallsbinderjava/MainActivity.java
+++ app/src/main/java/br/com/fabriciocurvello/appremoteprocedurecallsbinderjava/MainActivity.java
@@ -1,6 +1,14 @@
 package br.com.fabriciocurvello.appremoteprocedurecallsbinderjava;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +17,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MyService mService;
+    private boolean mBound = false;
+
+    private Button bindServiceBtn;
+    private TextView messageTv;
+
+    // Define callbacks para o service binding, passados para bindService()
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            // Ligamos ao MyService, lançamos o IBinder e obtemos a instância do MyService
+            MyService.LocalBinder binder = (MyService.LocalBinder) service;
+            mService = binder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mBound = false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +50,38 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        bindServiceBtn.findViewById(R.id.btn_bind_service);
+        messageTv.findViewById(R.id.tv_message);
+
+        bindServiceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mBound) {
+                    String message = mService.getMessage();
+                    messageTv.setText(message);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Vincula ao LocalService
+        Intent intent = new Intent(this, MyService.class);
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        //Desvincula-se do serviço
+        if (mBound) {
+            unbindService(connection);
+            mBound = false;
+        }
     }
 }
---
+++ app/src/main/java/br/com/fabriciocurvello/appremoteprocedurecallsbinderjava/MyService.java
@@ -0,0 +1,33 @@
+package br.com.fabriciocurvello.appremoteprocedurecallsbinderjava;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+public class MyService extends Service {
+
+    // Binder entregue aos clientes
+    private final IBinder binder = new LocalBinder();
+
+    // Classe utilizada para o cliente Binder
+    public class LocalBinder extends Binder {
+        MyService getService() {
+            return MyService.this;
+        }
+    }
+
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        //throw new UnsupportedOperationException("Not yet implemented");
+        return binder;
+    }
+
+    // Um método simples que os clientes podem chamar
+    public String getMessage() {
+        return "Hello from the service!";
+    }
+}
